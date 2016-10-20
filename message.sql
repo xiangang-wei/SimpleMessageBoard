4f1d25dd9e0ff3086202c5896d08e9d00a9ec5a9
xiangang.wei <xiangang.wei@feixun.com.cn>
2016-10-20 11:32:44 +0800

    modify;

--- message.sql
+++ message.sql
@@ -2,7 +2,7 @@ drop table if exists message;
 create table message(
 	id int not null auto_increment,
 	author varchar(255) not null,
-	name varchar(255) not null,
+	title varchar(255) not null,
 	time varchar(255) not null,
 	content text not null,
 	primary key(id)
